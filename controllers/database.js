d1818b3705bf1ae2c0b59b654c49aaa2c0792758
Cristianmen <32776012+Cristianmen@users.noreply.github.com>
2021-04-07 09:27:11 -0500

    primer commit

---
+++ controllers/database.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const AWS = require('aws-sdk'),
+    USERS_TABLE = process.env.USERS_TABLE;
+let params;
+
+class DynamoDBClass {
+    dynamoDB;
+
+    constructor() {
+        this.dynamoDB = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+    };
+
+    async putUser(data) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                params = {};
+                params.TableName = USERS_TABLE;
+                params.Item = data
+
+                console.log('putUser -> params:  ', params);
+                const resul = await this.dynamoDB.put(params).promise();
+                console.log('putUser -> resul:  ', resul);
+                resolve(resul);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+    async queryUser(value) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                params = {};
+                params.TableName = USERS_TABLE;
+                params.Key = { userId: value};
+
+                console.log('putUser -> params:  ', params);
+                const resul = await this.dynamoDB.get(params).promise();
+                console.log('putUser -> resul:  ', resul);
+                resolve(resul);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    async scanUser() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                params = {};
+                params.TableName = USERS_TABLE;
+                params.ProjectionExpression = 'userId';
+
+                console.log('putUser -> params:  ', params);
+                const resul = await this.dynamoDB.scan(params).promise();
+                console.log('putUser -> resul:  ', resul);
+                resolve(resul);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+}
+
+module.exports = DynamoDBClass;
---
+++ index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const databaseCtrl = require('./controllers/database');
+
+let usersDB = null,
+  data;
+
+async function query(event) {
+  console.log('event: query -> ', event);
+  usersDB = new databaseCtrl();
+  try {
+    if (event.pathParameters) {
+      const path = event.pathParameters.user;
+      console.log('path -> ', path);
+      const resulquery = await usersDB.queryUser(path);
+      return responseHtt(200, 'peticions exitosa/queryUser', resulquery);
+    } else {
+      const resulquery = await usersDB.scanUser();
+      return responseHtt(200, 'peticions exitosa/scanUser', resulquery);
+    }
+  } catch (error) {
+    console.log('catch/error: ', error);
+    return responseHtt(500, 'Error', error);
+  }
+
+
+};
+
+async function create(event) {
+
+  console.log('create: create -> ', event);
+  data = JSON.parse(event.body);
+  try {
+    usersDB = new databaseCtrl();
+    const userId = data.user,
+      name = data.name;
+
+    // const resulInsert = await usersDB.queryUser( userId);
+    const resulInsert = await usersDB.putUser({ userId, name });
+    // const resulInsert = await usersDB.scanUser();
+    console.log('resulInsert: ', typeof resulInsert);
+    console.log('resulInsert: ', resulInsert);
+
+
+
+    // resulInsert.then(() => {
+    //   console.log('then: ', then);
+    // }).catch((error) => {
+
+    //   console.log('error: ', error);
+    // });
+
+    return responseHtt(200, 'peticions exitosa', resulInsert);
+
+
+  } catch (error) {
+    console.log('catch/error: ', error);
+    return responseHtt(500, 'Error', error);
+  }
+};
+
+function responseHtt(status, message, output) {
+  return {
+    statusCode: status,
+    body: JSON.stringify(
+      {
+        message: message,
+        input: output,
+      },
+      null,
+    ),
+  };
+
+
+}
+
+
+module.exports.query = query;
+module.exports.create = create;
